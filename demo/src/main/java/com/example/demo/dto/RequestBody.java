cc2d767340d817123662329f6abe7d840ef5e11b
phonesavanh2229 <motlasa7@gmail.com>
2025-07-24 09:50:54 +0700

    add class response new

--- demo/src/main/java/com/example/demo/dto/RequestBody.java
+++ demo/src/main/java/com/example/demo/dto/RequestBody.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.example.demo.dto;
 
 /**
  *
  * @author stl_sdd_phonesavanh
  */
+@Body
 public class RequestBody {
     
 }
